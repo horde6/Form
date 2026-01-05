cdae6ee3b59047de5dc9bb7bcb6bca5464d96ae9
Dmitry Petrov <dpetrov67@gmail.com>
2026-01-04 21:53:27 -0500

    V3: Improve credit card validation checks

--- src/V3/CreditcardVariable.php
+++ src/V3/CreditcardVariable.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Horde\Form\V3;
+
 use Horde_Variables;
 use Horde_Form_Translation;
 
@@ -13,7 +15,7 @@ class CreditcardVariable extends BaseVariable
 
         if (!empty($value)) {
             /* getCardType() will also verify the checksum. */
-            $type = $this->getCardType($value);
+            $type = self::getCardType($value);
             if ($type === false || $type == 'unknown') {
                 return $this->invalid('This does not seem to be a valid card number.');
             }
@@ -22,75 +24,78 @@ class CreditcardVariable extends BaseVariable
         return true;
     }
 
-    //TODO: Improve
-    public function getChecksum($ccnum)
+    public static function getChecksum(string $ccnum): int
     {
+
         $len = strlen($ccnum);
-        if (!is_long($len / 2)) {
-            $weight = 2;
-            $digit = $ccnum[0];
-        } else {
-            $weight = 1;
-            $digit = $ccnum[0] * 2;
-        }
-        if ($digit > 9) {
-            $digit = $digit - 9;
-        }
-        $i = 1;
-        $checksum = $digit;
-        while ($i < $len) {
-            if ($ccnum[$i] != ' ') {
-                $digit = $ccnum[$i] * $weight;
-                $weight = ($weight == 1) ? 2 : 1;
+        $checksum = 0;
+        $double = false;
+
+        for ($i = $len - 1; $i >= 0; --$i) {
+            $digit = (int) $ccnum[$i];
+
+            if ($double) {
+                $digit *= 2;
                 if ($digit > 9) {
-                    $digit = $digit - 9;
+                    $digit -= 9;
                 }
-                $checksum += $digit;
             }
-            $i++;
+
+            $checksum += $digit;
+            $double = !$double;
         }
 
         return $checksum;
     }
 
-    public function getCardType($ccnum)
+    public static function getCardType(string $ccnum)
     {
-        $sum = $this->getChecksum($ccnum);
+        // Remove spaces and other non-digits
+        $ccnum = preg_replace('/\D/', '', $ccnum);
+
         $l = strlen($ccnum);
 
-        // Screen checksum.
-        if (($sum % 10) != 0) {
+        // Screen checksum first
+        if ($l === 0 || self::getChecksum($ccnum) % 10 !== 0) {
             return false;
         }
 
-        // Check for Visa.
-        if ((($l == 16) || ($l == 13)) &&
-            ($ccnum[0] == 4)) {
+        // Check for Visa
+        if (
+            ($l === 13 || $l === 16) &&
+            $ccnum[0] === '4'
+        ) {
             return 'visa';
         }
 
-        // Check for MasterCard.
-        if (($l == 16) &&
-            ($ccnum[0] == 5) &&
-            ($ccnum[1] >= 1) &&
-            ($ccnum[1] <= 5)) {
+        // Check for MasterCard (51–55)
+        if (
+            $l === 16 &&
+            $ccnum[0] === '5' &&
+            $ccnum[1] >= '1' &&
+            $ccnum[1] <= '5'
+        ) {
             return 'mastercard';
         }
 
-        // Check for Amex.
-        if (($l == 15) &&
-            ($ccnum[0] == 3) &&
-            (($ccnum[1] == 4) || ($ccnum[1] == 7))) {
+        // Check for Amex (34, 37)
+        if (
+            $l === 15 &&
+            $ccnum[0] === '3' &&
+            ($ccnum[1] === '4' || $ccnum[1] === '7')
+        ) {
             return 'amex';
         }
 
-        // Check for Discover (Novus).
-        if (strlen($ccnum) == 16 &&
-            substr($ccnum, 0, 4) == '6011') {
+        // Check for Discover (6011)
+        if (
+            $l === 16 &&
+            substr($ccnum, 0, 4) === '6011'
+        ) {
             return 'discover';
         }
 
-        // If we got this far, then no card matched.
+        // If we got this far, then no card matched
         return 'unknown';
     }
 
