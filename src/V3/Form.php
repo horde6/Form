f6109eace21489d50920c032afaaf98d44ffccc9
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-08 10:15:07 +0100

    feat(api): create distinct migration and V3-native interfaces
    Split existing interfaces into separate migration and V3-native interfaces
    to clearly distinguish lib/ compatibility methods from V3-specific additions.
    
    - Clear documentation of which methods existed in lib/ vs new in V3
    - Easier migration path - developers can see exactly what's supposed to be compatible and what isn't
    - No changes to implementing classes (BaseVariable, BaseForm, BaseAction)
    - Interfaces use extends, so existing code using Variable/Form/Action continues to work
    - Foundation for development of V3 without disturbing migration support
    
    No breaking changes to public API.

--- src/V3/Form.php
+++ src/V3/Form.php
@@ -1,6 +1,30 @@
 <?php
+/**
+ * Copyright 2001-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL
+ * @package  Form
+ */
+
 namespace Horde\Form\V3;
 
-interface Form
+/**
+ * Combined interface for forms.
+ *
+ * Extends both FormMigrationInterface (lib/ compatibility methods)
+ * and FormV3Interface (V3-native methods) to provide the complete
+ * Form API.
+ *
+ * Implementations of this interface support both migrated code from lib/
+ * and new V3-specific functionality.
+ *
+ * @category Horde
+ * @package  Form
+ */
+interface Form extends FormMigrationInterface, FormV3Interface
 {
 }
